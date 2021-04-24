30c04f289e38ea679876c7c0f111dbe11a11414a
stz0117 <44522321+stz0117@users.noreply.github.com>
2021-04-24 23:42:27 +0800

    Revert "update"

--- fetch/api.js
+++ fetch/api.js
@@ -1,14 +1,10 @@
-import axios from 'axios'
-import {
-	getEncryptedPassword
-} from "../common/encrypt.js"
+	import axios from 'axios'
 
 axios.defaults.timeout = 10000;
-axios.defaults.baseURL = '';
+axios.defaults.baseURL = '/api';
 
 // axios请求拦截器，统一处理request
 axios.interceptors.request.use((config) => {
-	config.headers.Authorization = "Bearer " + uni.getStorageSync('jwt')
 	return config;
 }, (error) => {
 	return Promise.reject(error);
@@ -16,15 +12,6 @@ axios.interceptors.request.use((config) => {
 
 // axios返回结果拦截器，返回状态判断
 axios.interceptors.response.use((res) => {
-	if(res.data.data.jwt!=undefined){
-		uni.setStorage({
-		    key: 'jwt',
-		    data: res.data.data.jwt,
-		    success: function () {
-		        console.log('jwt凭据保存成功');
-		    }
-		});
-	}
 	return res;
 }, (error) => {
 	return Promise.reject(error);
@@ -47,63 +34,8 @@ function fetchGet(url, param) {
 	})
 }
 
-function fetchPost(url, data) {
-	return new Promise((resolve, reject) => {
-		axios.post(url, data)
-			.then(response => {
-				console.log("responsed")
-				resolve(response.data)
-			}, err => {
-				reject(err)
-			})
-			.catch((error) => {
-				reject(error);
-			})
-	})
-}
-
-/*基本功能API*/
-export function getStatic(path) {
-	return fetchGet(`/static`+path)
-}
-
-
-/*用户权限管理API*/
-export function postLoginIn(userPhone, password) {
-	var data = {
-		"userPhone": userPhone,
-		"password": getEncryptedPassword(password)
-	}
-	return fetchPost(`/api/login`, data = data);
-}
-
-export function postJoinIn(userPhone, userName, password, authType = 1, userEmail) {
-	var data = {
-		"userPhone": userPhone,
-		"userName": userName,
-		"password": getEncryptedPassword(password),
-		"authType": authType
-	}
-	if (userEmail !== undefined) {
-		data.userEmail = userEmail
-	}
-	return fetchPost(`/api/join`, data = data);
-}
-
-export function getUserInfo(userPhone){
-	return fetchGet("/api/user/info/"+userPhone)
-}
-
-
-/*在线药房模块API*/
-export function getPharBoothList(cata, count){
-	const params = {
-		"cata": cata,
-		"count": count
-	}
-	return fetchGet(`/api/phar/booth/list`, params)
-}
-
-export function getPharBoothDetail(id){
-	return fetchGet(`/api/phar/booth/detail/`+id)
+// 对应的后端接口
+export function ping() {
+	console.log("ping started")
+	return fetchGet(`/ping`);
 }
